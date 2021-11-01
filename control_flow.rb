ec17bcf20df6ed565aa70bee3d952e96bfae61b7
Aristela Marku <aristela.marku@gmail.com>
2021-11-01 09:43:58 +0100

    commit tree 61ebf6168f6d8f4fd5e8a72a2641c6ae98cf2286

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,53 @@
 def admin_login(username, password)
-  # your code here
+  if((username == "admin" || username == "ADMIN")&&password=="12345")
+    "Access granted"
+  else
+    "Access denied"
+  end
+
 end
 
-def hows_the_weather(temperature)
-  # your code here
+def hows_the_weather(num)
+  if (num<40)
+    "It's brisk out there!"
+  elsif(num>40&&num<65)
+    "It's a little chilly out there!"
+  elsif(num>85)
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+end
 end
 
-def fizzbuzz(num)
-  # your code here
-end
+
 
 def calculator(operation, num1, num2)
-  # your code here
+ case (operation)
+when "+"
+  num1+num2
+when "-"
+  num1-num2
+when "*"
+  num1*num2
+when "/"
+  num1/num2
+else "nope"
+  puts'Invalid operation!'
+   
+
+end
 end
 
+
+
+def fizzbuzz num
+if num %3==0 && num %5==0
+  return "FizzBuzz"
+elsif num %3==0
+  "Fizz"
+elsif num %5==0
+  "Buzz"
+else
+   num
+end
+end
